af52050a353ecffa0c8dfee4b5ecb0cd2fb6a4b7
jeong-soo <wjdtn1075@naver.com>
2021-04-28 03:06:56 +0900

    ft_atoi.c upadte 2021/04/28

--- 1.libft/ft_atoi.c
+++ 1.libft/ft_atoi.c
@@ -3,15 +3,28 @@ int ft_atoi(char *str)
 	int sign;
 	int result;
 
-	while(*str++)
+	sign = 1;
+	result = 0;
+
+	while(*str)
 	{
-		if(*str == '+' || *str == '-')
+		if((*str >= 9 && *str <= 13) || *str == 32)
+			str++;
+		else if(*str == '+' || *str == '-')
 		{
 			if(*str == '-')
-1
-}
-int main()
-{
-	char str[] = "-486";
-	printf("%d", ft_atoi(str));
+			{
+				sign *= -1;
+				}
+			str++;
+		}
+		else if(*str >= '0' && *str <= '9')
+		{
+			result = result * 10 + *str - '0';
+			str++;
+		}
+		else
+			break;
+	}
+	return (sign * result);
 }
